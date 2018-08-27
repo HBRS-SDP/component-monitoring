df8cdf69713ff0eb0638d0980035272b0ed2bf5e
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-08-27 17:08:59 +0200

    [laser_device_monitor] The monitor is now implemented

--- component_monitoring/monitors/hardware/laser/laser_device_monitor.py
+++ component_monitoring/monitors/hardware/laser/laser_device_monitor.py
@@ -1,18 +1,27 @@
+from os import stat
+
 from component_monitoring.monitor_base import MonitorBase
 
 class LaserDeviceMonitor(MonitorBase):
     def __init__(self, config_params):
         super(LaserDeviceMonitor, self).__init__(config_params)
-        self.device_names = list()
-        self.device_status_names = list()
+        self.dev_names = list()
+        self.dev_status_names = list()
         for mapping in config_params.mappings:
-            self.device_names.append(mapping.inputs[0])
-            self.device_status_names.append(mapping.outputs[0].name)
+            self.dev_names.append(mapping.inputs[0])
+            self.dev_status_names.append(mapping.outputs[0].name)
 
     def get_status(self):
         status_msg = self.get_status_message_template()
         status_msg['monitorName'] = self.config_params.name
         status_msg['healthStatus'] = dict()
-        for i, device_name in enumerate(self.device_names):
-            status_msg['healthStatus'][self.device_status_names[i]] = False
+        for i, dev_name in enumerate(self.dev_names):
+            status_msg['healthStatus'][self.dev_status_names[i]] = self.__device_exists(dev_name)
         return status_msg
+
+    def __device_exists(self, dev_name):
+        try:
+            stat(dev_name)
+            return True
+        except OSError:
+            return False
