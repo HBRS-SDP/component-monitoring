1c02b393cdc230ea47ddcc2b36b20e95e99f7096
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2019-04-19 12:09:19 +0200

    [config_utils] Added more helpers and refactored the main script to use those

--- component_monitoring/config/config_utils.py
+++ component_monitoring/config/config_utils.py
@@ -1,7 +1,57 @@
 from os import listdir
 from os.path import join, isfile
+import yaml
+
+from component_monitoring.config.config_file_reader import ComponentMonitorConfigFileReader
 
 class ConfigUtils(object):
+    # a dictionary of configuration parameters for the component monitoring application
+    config_data = None
+
+    # a list of component_monitoring.config.config_params.ComponentMonitorConfig
+    # objects representing the configurations of all hardware monitors
+    hw_monitor_config_params = []
+
+    # a list of component_monitoring.config.config_params.ComponentMonitorConfig
+    # objects representing the configuration of all software monitors
+    sw_monitor_config_params = []
+
+    @staticmethod
+    def read_config(config_file_path):
+        '''Reads a YAML configuration file containing parameters for the
+        component monitoring application and returns a dictionary with the
+        file contents.
+
+        Keyword arguments:
+        config_file_path: str -- path to a YAML configuration file of the component
+                                 monitoring application
+
+        '''
+        config_data = {}
+        with open(config_file_path, 'r') as config_file:
+            config_data = yaml.load(config_file)
+        return config_data
+
+    @staticmethod
+    def get_config_params(monitor_config_dir):
+        '''Returns a list of component_monitoring.config.config_params.ComponentMonitorConfig
+        objects representing the configuration of the monitors specified
+        in the given directory.
+
+        Keyword arguments:
+        monitor_config_dir: str -- a directory containing component monitor
+                                   configuration files
+
+        '''
+        config_files = ConfigUtils.get_file_names_in_dir(monitor_config_dir)
+        monitor_config_params = []
+        for config_file in config_files:
+            print('Reading parameters of monitor {0}'.format(config_file))
+            component_config_params = ComponentMonitorConfigFileReader.load(monitor_config_dir,
+                                                                            config_file)
+            monitor_config_params.append(component_config_params)
+        return monitor_config_params
+
     @staticmethod
     def get_file_names_in_dir(dir_name):
         '''Returns a list file names in the given directory.
@@ -16,3 +66,53 @@ class ConfigUtils(object):
             if isfile(f_path):
                 file_names.append(f_name)
         return file_names
+
+    @staticmethod
+    def get_component_dependencies(component_name):
+        '''Returns a list with names of the components on which
+        the given component depends.
+
+        Raises a ValueError if the given component is unknown.
+
+        Keyword arguments:
+        component_name: str -- name of a component
+
+        '''
+        component_config = ConfigUtils.get_component_config(component_name)
+        if not component_config:
+            raise ValueError('[get_component_dependencies] Unknown component {0}'.format(component_name))
+        return component_config.component_dependencies
+
+    @staticmethod
+    def get_component_config(component_name):
+        '''Returns a component_monitoring.config.config_params.ComponentMonitorConfig
+        object representing the
+
+        Raises a ValueError if either "ConfigUtils.hw_monitor_config_params" or
+        "ConfigUtils.sw_monitor_config_params" is None.
+
+        Keyword arguments:
+        component_name: str -- name of a component
+
+        '''
+        component_config = None
+
+        if not ConfigUtils.hw_monitor_config_params or not ConfigUtils.sw_monitor_config_params:
+            raise AssertionError('ConfigUtils parameters not set')
+
+        # we look for the component in the hardware components
+        for config in ConfigUtils.hw_monitor_config_params:
+            if config.component_name == component_name:
+                component_config = config
+                break
+
+        if component_config:
+            return component_config
+
+        # if the component could not be found in the hardware components,
+        # we look for it in the software components
+        for config in ConfigUtils.sw_monitor_config_params:
+            if config.component_name == component_name:
+                component_config = config
+                break
+        return component_config
--- main.py
+++ main.py
@@ -4,35 +4,49 @@ import time
 import json
 import uuid
 import argparse
-import yaml
 
 from ropod.pyre_communicator.base_class import RopodPyre
-from component_monitoring.config.config_file_reader import ComponentMonitorConfigFileReader
 from component_monitoring.config.config_utils import ConfigUtils
 from component_monitoring.monitor_manager import MonitorManager
 from component_monitoring.utils.robot_store_interface import RobotStoreInterface
 from component_monitoring.communication import BlackBoxPyreCommunicator
 
-def get_config_data(config_file_path):
-    config_data = {}
-    with open(config_file_path, 'r') as config_file:
-        config_data = yaml.load(config_file)
-    return config_data
+def generate_robot_status_msg(robot_id):
+    '''Returns a status message dictionary with the following format:
 
-def generate_robot_msg(status_msg, robot_id):
+    {
+        "header":
+        {
+            "type": "HEALTH-STATUS",
+            "metamodel": "ropod-msg-schema.json",
+            "msgId": <unique-message-ID>,
+            "timestamp": <current-timestamp>
+        },
+        "payload":
+        {
+            "metamodel": "ropod-component-monitor-schema.json",
+            "robotId": [robot_id],
+            "monitors": {}
+        }
+    }
+
+    Keyword arguments:
+    robot_id: str -- robot ID/name for the status message
+
+    '''
     msg = dict()
+
     msg["header"] = dict()
     msg["header"]["type"] = "HEALTH-STATUS"
     msg["header"]["metamodel"] = "ropod-msg-schema.json"
     msg["header"]["msgId"] = str(uuid.uuid4())
     msg["header"]["timestamp"] = time.time()
-    payload = dict()
-    payload["metamodel"] = "ropod-component-monitor-schema.json"
-    payload["robotId"] = robot_id
-    payload["monitors"] = status_msg
-    msg["payload"] = payload
-    return json.dumps(msg)
 
+    msg["payload"] = dict()
+    msg["payload"]["metamodel"] = "ropod-component-monitor-schema.json"
+    msg["payload"]["robotId"] = robot_id
+    msg["payload"]["monitors"] = {}
+    return msg
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Monitor component status',
@@ -44,71 +58,59 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     config_file_path = args.config_file
-    config_data = get_config_data(config_file_path)
-    robot_id = config_data['robot_id']
+    config_data = ConfigUtils.read_config(config_file_path)
 
+    robot_id = config_data['robot_id']
     hw_monitor_config_dir = config_data['config_dirs']['hardware']
     sw_monitor_config_dir = config_data['config_dirs']['software']
 
-    hw_config_files = ConfigUtils.get_file_names_in_dir(hw_monitor_config_dir)
-    hw_monitor_config_params = list()
-    for config_file in hw_config_files:
-        print('Reading parameters of hardware monitor {0}'.format(config_file))
-        component_config_params = ComponentMonitorConfigFileReader.load(hw_monitor_config_dir,
-                                                                        config_file)
-        hw_monitor_config_params.append(component_config_params)
+    # we read the parameters of the hardware and software monitors
+    hw_monitor_config_params = ConfigUtils.get_config_params(hw_monitor_config_dir)
+    sw_monitor_config_params = ConfigUtils.get_config_params(sw_monitor_config_dir)
 
-    sw_config_files = ConfigUtils.get_file_names_in_dir(sw_monitor_config_dir)
-    sw_monitor_config_params = list()
-    for config_file in sw_config_files:
-        print('Reading parameters of software monitor {0}'.format(config_file))
-        component_config_params = ComponentMonitorConfigFileReader.load(sw_monitor_config_dir,
-                                                                        config_file)
-        sw_monitor_config_params.append(component_config_params)
+    # we populate the parameters of the configuration utilities
+    # to simplify runtime access to the configuration data
+    ConfigUtils.config_data = config_data
+    ConfigUtils.hw_monitor_config_params = hw_monitor_config_params
+    ConfigUtils.sw_monitor_config_params = sw_monitor_config_params
 
-    pyre_comm = RopodPyre(robot_id, config_data['status_communication']['zyre_groups'], [])
-    pyre_comm.start()
-
-    robot_store_interface = RobotStoreInterface(db_name=config_data['robot_store_interface']['db_name'],
-                                                monitor_collection_name=config_data['robot_store_interface']['monitor_collection_name'],
-                                                db_port=config_data['robot_store_interface']['db_port'])
+    # we initialise a communicator for querying the black box
     black_box_comm = BlackBoxPyreCommunicator(config_data['black_box']['zyre_node_name'],
                                               config_data['black_box']['zyre_groups'],
                                               config_data['black_box']['id'])
+
+    # we create a status communicator
+    pyre_comm = RopodPyre(robot_id, config_data['status_communication']['zyre_groups'], [])
+    pyre_comm.start()
+
+    # we create an interface to the robot store interface for saving the status
+    robot_store_interface = RobotStoreInterface(db_name=config_data['robot_store_interface']['db_name'],
+                                                monitor_collection_name=config_data['robot_store_interface']['monitor_collection_name'],
+                                                db_port=config_data['robot_store_interface']['db_port'])
+
+    # we initialise a manager for the monitors that will continuously
+    # update the component status message
     monitor_manager = MonitorManager(hw_monitor_config_params,
                                      sw_monitor_config_params,
                                      robot_store_interface,
                                      black_box_comm)
+
+    # we initialise an overall status message that will continuously
+    # be updated with the component statuses
+    overall_status_msg = generate_robot_status_msg(robot_id)
     try:
         while True:
-            status_msg = monitor_manager.monitor_components()
-            robot_store_interface.store_monitor_msg(status_msg)
+            component_status_msg = monitor_manager.monitor_components()
+            robot_store_interface.store_monitor_msg(component_status_msg)
 
-            robot_msg = generate_robot_msg(status_msg, robot_id)
-            if (args.debug):
-                print(json.dumps(status_msg, indent=2))
-            pyre_comm.shout(robot_msg)
+            overall_status_msg["header"]["timestamp"] = time.time()
+            overall_status_msg["payload"]["monitors"] = component_status_msg
+            if args.debug:
+                print(json.dumps(overall_status_msg, indent=2))
+            pyre_comm.shout(overall_status_msg)
             time.sleep(0.5)
     except (KeyboardInterrupt, SystemExit):
         print('Component monitors exiting')
         pyre_comm.shutdown()
         monitor_manager.stop_monitors()
         black_box_comm.shutdown()
-
-    ### debugging printout
-    # hardware_monitor_config_params = ComponentMonitorConfigFileReader.load(hw_monitor_config_dir_name,
-    #                                                                          "laser_monitor.yaml");
-    # print('Monitor name: {0}'.format(hardware_monitor_config_params.name))
-    # for mode_params in hardware_monitor_config_params.modes:
-    #     print('    Mode name: {0}'.format(mode_params.name))
-    #     for fn_mapping_params in mode_params.mappings:
-    #         print('    Inputs:\n')
-    #         for monitor_input in fn_mapping_params.inputs:
-    #             print('        {0}'.format(monitor_input))
-    #
-    #         print('    Outputs:\n')
-    #         for monitor_output in fn_mapping_params.outputs:
-    #             print('        {0}'.format(monitor_output.name))
-    #             print('        {0}'.format(monitor_output.obtained_value_type))
-    #             print('        {0}'.format(monitor_output.expected_value))
-    #     print()
