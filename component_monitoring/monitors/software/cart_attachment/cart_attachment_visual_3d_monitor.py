1896dcba242734aa4f770befdf0e575bfba3c700
Santosh Thoduka <santosh.thoduka@gmail.com>
2019-09-16 09:36:51 +0200

    add cart attachment monitor

---
+++ component_monitoring/monitors/software/cart_attachment/cart_attachment_visual_3d_monitor.py
@@ -0,0 +1,77 @@
+from component_monitoring.monitor_base import MonitorBase
+import rospy
+import geometry_msgs.msg
+import numpy as np
+from multiprocessing import Process, Manager
+
+class CartAttachmentVisual3DMonitor(MonitorBase):
+    def __init__(self, config_params, black_box_comm):
+        super(CartAttachmentVisual3DMonitor, self).__init__(config_params, black_box_comm)
+        self.output_names = list()
+        for output in config_params.mappings[0].outputs:
+            self.output_names.append(output.name)
+
+        self.cart_pose_topic_name = config_params.arguments.get('cart_pose_topic_name', '/cart_plane_detector/plane_pose')
+        self.cart_attached_param_name = config_params.arguments.get('cart_attached_param_name',
+                                                            '/manuever_navigation/is_load_attached')
+        self.nominal_plane_distance = config_params.arguments.get('nominal_plane_distance', 0.55)
+        self.plane_distance_tolerance = config_params.arguments.get('plane_distance_tolerance', 0.05)
+        self.last_received_time = None
+        self.last_cart_pose = None
+        self.node_initialised = False
+
+    def get_status(self):
+        try:
+            rospy.get_master().getPid()
+            if not self.node_initialised:
+                self.node_thread = Process(target=self.__create_node)
+                self.node_thread.start()
+                self.node_initialised = True
+        except:
+            if self.node_initialised:
+                rosnode.kill_nodes('cart_attachment_monitor')
+                self.node_thread.terminate()
+            self.node_initialised = False
+
+        status_msg = self.get_status_message_template()
+        status_msg["monitorName"] = self.config_params.name
+        status_msg["monitorDescription"] = self.config_params.description
+        status, output = self._get_cart_status()
+        status_msg["healthStatus"] = dict()
+        status_msg["healthStatus"]["status"] = status
+        status_msg["healthStatus"][self.output_names[0]] = output
+        return status_msg
+
+    def __create_node(self):
+        rospy.init_node('cart_attachment_monitor', disable_signals=True)
+        self.__init_subscribers()
+        rospy.spin()
+
+    def __init_subscribers(self):
+        self.cart_pose_sub = rospy.Subscriber(self.cart_pose_topic_name, geometry_msgs.msg.PoseStamped, self.pose_callback)
+
+    def pose_callback(msg):
+        self.last_cart_pose = msg.pose
+        self.last_received_time = rospy.Time.now()
+
+    def _get_cart_status(self):
+        """
+        If cart is not supposed to be attached, returns True
+        If cart is supposed to be attached, checks last known distance of cart and
+        returns True if cart is within threshold
+        :returns: (bool, bool)
+
+        """
+        is_cart_attached = rospy.get_param(self.cart_attached_param_name, False)
+        if not is_cart_attached:
+            return True, True
+        if self.last_cart_pose is None or self.last_received_time is None or \
+           (rospy.Time.now() - self.last_received_time) > rospy.Duration(5.0):
+            return False, False
+
+        dist = np.linalg.norm([pose.postion.x, pose.position.y])
+        if (dist < self.nominal_plane_distance + self.plane_distance_tolerance and
+            dist > self.nominal_plane_distance - self.plane_distance_tolerance):
+            return True, True
+        else:
+            return False, False
