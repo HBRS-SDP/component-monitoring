125e78030304fd8279c39321b23b157ab6b11ab0
Santosh Thoduka <santosh.thoduka@gmail.com>
2019-09-19 09:53:49 +0200

    [cart attachment monitor] toggle cart publisher if we don't receive any cart poses in time

--- component_monitoring/monitors/software/cart_attachment/cart_attachment_visual_3d_monitor.py
+++ component_monitoring/monitors/software/cart_attachment/cart_attachment_visual_3d_monitor.py
@@ -1,9 +1,10 @@
 from component_monitoring.monitor_base import MonitorBase
-import rospy
-import geometry_msgs.msg
 import numpy as np
 import time
 from multiprocessing import Process, Manager
+import rospy
+import geometry_msgs.msg
+from ropod_ros_msgs.srv import ToggleObjectPublisher
 
 class CartAttachmentVisual3DMonitor(MonitorBase):
     def __init__(self, config_params, black_box_comm):
@@ -15,6 +16,7 @@ class CartAttachmentVisual3DMonitor(MonitorBase):
         self.cart_pose_topic_name = config_params.arguments.get('cart_pose_topic_name', '/cart_plane_detector/plane_pose')
         self.cart_attached_param_name = config_params.arguments.get('cart_attached_param_name',
                                                             '/manuever_navigation/is_load_attached')
+        self.toggle_publisher_srv_name = config_params.arguments.get('toggle_publisher_srv_name', '/cart_plane_detector/toggle_object_publisher')
         self.nominal_plane_distance = config_params.arguments.get('nominal_plane_distance', 0.55)
         self.plane_distance_tolerance = config_params.arguments.get('plane_distance_tolerance', 0.05)
         self.process_manager = Manager()
@@ -48,10 +50,15 @@ class CartAttachmentVisual3DMonitor(MonitorBase):
     def __create_node(self):
         rospy.init_node('cart_attachment_monitor', disable_signals=True)
         self.__init_subscribers()
-        rospy.spin()
+        loop_rate = rospy.Rate(2.0)
+        while not rospy.is_shutdown():
+            loop_rate.sleep()
+
 
     def __init_subscribers(self):
         self.cart_pose_sub = rospy.Subscriber(self.cart_pose_topic_name, geometry_msgs.msg.PoseStamped, self.pose_callback)
+        self.toggle_cart_publisher_client = rospy.ServiceProxy(self.toggle_publisher_srv_name, ToggleObjectPublisher)
+        self.toggle_cart_publisher_client(enable_publisher=True)
 
     def pose_callback(self, msg):
         self.cart_state['cart_pose'] = msg.pose
@@ -71,6 +78,8 @@ class CartAttachmentVisual3DMonitor(MonitorBase):
 
         if self.cart_state['cart_pose'] is None or \
            (time.time() - self.cart_state['last_received_time']) > 10.0:
+            self.toggle_cart_publisher_client = rospy.ServiceProxy(self.toggle_publisher_srv_name, ToggleObjectPublisher)
+            self.toggle_cart_publisher_client(enable_publisher=True)
             return False, False
 
         dist = np.linalg.norm([self.cart_state['cart_pose'].position.x, self.cart_state['cart_pose'].position.y])
