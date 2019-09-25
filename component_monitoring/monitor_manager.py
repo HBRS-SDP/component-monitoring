e12b4b35aa0e9e129e972f6bbbb5e60e965773cd
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2019-09-25 16:16:20 +0200

    [monitor_manager] Added a lock while writing to the msg status dict

--- component_monitoring/monitor_manager.py
+++ component_monitoring/monitor_manager.py
@@ -34,6 +34,7 @@ class MonitorManager(object):
         self.monitor_status_msgs = dict()
         self.monitor_threads = dict()
         self.monitoring = False
+        self.monitor_status_dict_lock = threading.Lock()
         for component_id, monitors in self.component_monitor_data:
             monitor_msg = dict()
             component_name = self.component_descriptions[component_id]
@@ -57,8 +58,10 @@ class MonitorManager(object):
                 monitor_status = monitor.get_status()
                 monitor_msgs.append(monitor_status)
             self.robot_store_interface.store_component_status_msg(component_id, monitor_msgs)
+            self.monitor_status_dict_lock.acquire()
             self.monitor_status_msgs[component_id]['modes'] = monitor_msgs
             self.monitor_status_msgs[component_id]['component_sm_state'] = self.robot_store_interface.read_component_sm_status(component_id)
+            self.monitor_status_dict_lock.release()
             time.sleep(0.1)
 
     def get_component_status_list(self):
