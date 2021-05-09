e90e4930abd9b759ac720f9639f84cfb51a8e917
Michal <mlk04@wp.pl>
2021-05-09 17:46:56 +0200

    [component_monitoring] Receiving pointclo ind rgbd camera monitor implemented

--- component_monitoring/monitors/hardware/rgbd_camera/rgbd_camera_pointcloud_monitor_monitor.py
+++ component_monitoring/monitors/hardware/rgbd_camera/rgbd_camera_pointcloud_monitor_monitor.py
@@ -1,8 +1,15 @@
 from component_monitoring.monitor_base import MonitorBase
+import rospy
+from sensor_msgs.msg import PointCloud2
 
-class Rgbd_CameraPointcloud_MonitorMonitor(MonitorBase):
+class RgbdCameraPointcloudMonitorMonitor(MonitorBase):
     def __init__(self, config_params, black_box_comm):
-        super(Rgbd_CameraPointcloud_MonitorMonitor, self).__init__(config_params, black_box_comm)
+        super(RgbdCameraPointcloudMonitorMonitor, self).__init__(config_params, black_box_comm)
+        self._subscriber = rospy.Subscriber('/hsrb/head_rgbd_sensor/depth_registered/points', PointCloud2, self.callback)
+        self._pointcloud = None
+
+    def callback(self, data):
+        self._pointcloud = data.data
 
     def get_status(self):
         status_msg = self.get_status_message_template()
@@ -10,4 +17,8 @@ class Rgbd_CameraPointcloud_MonitorMonitor(MonitorBase):
         status_msg["monitorDescription"] = self.config_params.description
         status_msg["healthStatus"] = dict()
         status_msg["healthStatus"]["status"] = False
+        if self._pointcloud is not None:
+            rospy.loginfo("I got poincloud!")
+        else:
+            rospy.logwarn("Oh no I have no poincloud :-( .")
         return status_msg
