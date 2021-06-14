7961cff654624665ba02a257ae5264d0814f025b
Michal <mlk04@wp.pl>
2021-06-14 17:57:14 +0200

    [RGBDMonitor] Counting NAN values

--- component_monitoring/monitors/hardware/rgbd_camera/rgbd_camera_pointcloud_monitor_monitor.py
+++ component_monitoring/monitors/hardware/rgbd_camera/rgbd_camera_pointcloud_monitor_monitor.py
@@ -15,12 +15,21 @@ class RgbdCameraPointcloudMonitorMonitor(MonitorBase):
         self.producer = KafkaProducer(bootstrap_servers='localhost:9092')
         super(RgbdCameraPointcloudMonitorMonitor, self).__init__(config_params, black_box_comm)
         self._subscriber = rospy.Subscriber('/hsrb/head_rgbd_sensor/pointcloud', PointCloud2, self.callback)
+        self._nans_threshold_ratio = 0.65 # arbitrary chosen threshold
         self._pointcloud = None
+        self._status = True # monitor thinks that the component is healthy
 
     def callback(self, data):
-        gen = point_cloud2.read_points(data, field_names=("x", "y", "z")) # for yelding the errors
-        #gen = point_cloud2.read_points(data, field_names=("x", "y", "z"), skip_nans=True) # smart getting rid of NaNs
-        self._pointcloud = list(gen)
+        gen = point_cloud2.read_points(data, field_names=("x", "y", "z"))
+        self._pointcloud = np.array(list(gen))
+
+    def pointcloud_rejection_policy(self, pointcloud):
+        nans_counts_in_each_row = np.count_nonzero(np.isnan(pointcloud), axis=1)
+        nans_count = np.count_nonzero(nans_counts_in_each_row)
+
+        if nans_count > pointcloud.shape[0]*self._nans_threshold_ratio:
+            return True
+        return False
 
     def to_cpp(self, msg):
         """
@@ -50,10 +59,20 @@ class RgbdCameraPointcloudMonitorMonitor(MonitorBase):
                      "nans": False
                  }
                  }
-        if self._pointcloud:
-            if np.isnan(np.array(self._pointcloud)).any():
+                 
+
+        if not self._pointcloud is None:
+            self._status = True
+
+            rospy.loginfo("Poincloud from component received.")
+            
+            if self.pointcloud_rejection_policy(self._pointcloud):
+                self._status = False
+
                 event['healthStatus']['nans'] = True
-                rospy.logwarn("Detected NaN values in the pointcloud.")
+                rospy.logwarn("Number of NaN values in the pointcloud exceede a threshold of {}.".
+                format(int(self._nans_threshold_ratio*self._pointcloud.shape[0])))
+                
                 future = self.producer.send(self.topic_name, 
                                             json.dumps(event, 
                                             default=json_util.default).encode('utf-8'))
@@ -65,14 +84,15 @@ class RgbdCameraPointcloudMonitorMonitor(MonitorBase):
                                             json.dumps(event,
                                             default=json_util.default).encode('utf-8'))
                 result = future.get(timeout=60)
+        
+        else:
+            self._status = False
+            rospy.logwarn("No poincloud from component received.")
 
         status_msg = self.get_status_message_template()
         status_msg["monitorName"] = self.config_params.name
         status_msg["monitorDescription"] = self.config_params.description
         status_msg["healthStatus"] = dict()
-        status_msg["healthStatus"]["status"] = False
-        if self._pointcloud is not None:
-            rospy.loginfo("Poincloud from component received.")
-        else:
-            rospy.logwarn("No poincloud from component received.")
+        status_msg["healthStatus"]["status"] = self._status
+
         return status_msg
