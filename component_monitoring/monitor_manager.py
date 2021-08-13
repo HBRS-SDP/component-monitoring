75dc25c84fb3c1c92bcce34854714e5a48bf0ff4
Vincent Scharf <vincent.scharf@smail.inf.h-brs.de>
2021-08-13 12:47:34 +0200

    add pydoc for monitor_manager

--- component_monitoring/monitor_manager.py
+++ component_monitoring/monitor_manager.py
@@ -6,6 +6,7 @@ from typing import List, Optional, Dict
 from bson import json_util
 from jsonschema import validate
 from kafka import KafkaProducer, KafkaConsumer
+from kafka.consumer.fetcher import ConsumerRecord
 from kafka.producer.future import FutureRecordMetadata
 
 from component_monitoring.config.config_params import ComponentMonitorConfig
@@ -53,13 +54,14 @@ class MonitorManager(Process):
             self.component_descriptions[monitor_config.component_name] = monitor_config.description
             self.monitor_config[monitor_config.component_name] = monitor_config
 
-    def __send_control_message(self, msg) -> FutureRecordMetadata:
+    def __send_control_message(self, msg: Dict) -> FutureRecordMetadata:
         """
+        Send a control message over the control channel
 
-        @param msg:
-        @return:
+        @param msg: The control message as a JSON Dict
+        @return: FutureRecordMetadata: resolves to RecordMetadata
         """
-        result =  self.producer.send(self.control_channel, msg)
+        result = self.producer.send(self.control_channel, msg)
         return result
 
     def run(self) -> None:
@@ -96,40 +98,46 @@ class MonitorManager(Process):
                             print(topic)
                             response['monitors'].append({"name": monitor, "topic": topic})
                         except Exception as e:
-                            self.logger.warning(f"Monitor of component {component} with ID {monitor} could not be started!")
+                            self.logger.warning(
+                                f"Monitor of component {component} with ID {monitor} could not be started!")
                             response['monitors'].append({"name": monitor, "exception": e})
                             code = ResponseCode.FAILURE
                     self.send_response(component, code, response)
 
     def log_off(self) -> None:
         """
+        Inform the components, that the monitor manager is shutting down
 
-        @return:
+        @return: None
         """
         for component in self.monitors.keys():
             self.send_info(component, "manager shutting down")
 
     def serialize(self, msg) -> bytes:
         """
+        Serializes any message to be sent via the Kafka message bus
 
-        @param msg:
-        @return:
+        @param msg: JSON serializable message
+        @return: serialized message
         """
         return json.dumps(msg, default=json_util.default).encode('utf-8')
 
-    def deserialize(self, msg) -> dict:
+    def deserialize(self, msg: ConsumerRecord) -> dict:
         """
+        Deserializes any message received via the Kadka message bus
 
-        @param msg:
-        @return:
+        @param msg: A Kafka ConsumerRecord, whose message value should be deserialized. It is expected, that the message
+                    value is JSON serializable
+        @return: The message value as JSON Dict
         """
         return json.loads(msg.value)
 
     def validate_control_message(self, msg: dict) -> bool:
         """
+        Validate a control message against the respective schema.
 
-        @param msg:
-        @return:
+        @param msg: Message as JSON Dict to be validated
+        @return: True, if the message is valid against the schema, False otherwise.
         """
         try:
             validate(instance=msg, schema=self.event_schema)
@@ -150,9 +158,8 @@ class MonitorManager(Process):
     def stop_monitors(self) -> None:
         """
         Call stop method of all monitors. The stop method is used for cleanup
-        (specifically for shutting down pyre nodes)
 
-        :return: None
+        @return: None
 
         """
         for component_name, monitors in self.monitors.items():
@@ -161,20 +168,23 @@ class MonitorManager(Process):
 
     def stop_monitor(self, component_name: str, mode_name: str) -> None:
         """
+        Stop a single monitor, specified by its component and mode name
 
-        @param component_name:
-        @param mode_name:
-        @return:
+        @param component_name: The name of the component the monitor belongs to
+        @param mode_name: The mode name of the monitor
+        @return: None
         """
         self.monitors[component_name][mode_name].terminate()
         del self.monitors[component_name][mode_name]
 
     def start_monitor(self, component_name: str, mode_name: str) -> Optional[str]:
         """
+        Start a single monitor, specified by its component and mode name
 
-        @param component_name:
-        @param mode_name:
-        @return:
+        @param component_name: The name of the component requesting monitoring
+        @param mode_name: The mode name of the monitor to be started
+        @return: If the monitor does not exist yet, returns the event topic the monitor is publishing on. Else, None is
+        returned.
         """
         if component_name in self.monitors and mode_name in self.monitors[component_name]:
             self.logger.warning(f"Monitor {mode_name} of {component_name} is already started!")
@@ -190,7 +200,14 @@ class MonitorManager(Process):
         monitor.run()
         return monitor.event_topic
 
-    def send_info(self, receiver: str, info: str):
+    def send_info(self, receiver: str, info: str) -> None:
+        """
+        Send an INFO message over the control channel.
+
+        @param receiver: The component the message is addressed to
+        @param info: The information string to be sent
+        @return: None
+        """
         message = dict()
         message['from'] = self._id
         message['to'] = receiver
@@ -201,11 +218,12 @@ class MonitorManager(Process):
 
     def send_response(self, receiver: str, code: ResponseCode, msg: Optional[Dict]) -> None:
         """
+        Send a RESPONSE message over the control channel
 
-        @param receiver:
-        @param code:
-        @param msg:
-        @return:
+        @param receiver: The component the message is addressed to
+        @param code: The ResponseCode to be sent
+        @param msg: The optional message to be included in the response
+        @return: None
         """
         message = dict()
         message['from'] = self._id
